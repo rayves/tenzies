602d7409154d31767e918bab8380b3738014d468
rayves <vo.raymond.v@outlook.com>
2022-08-18 03:11:12 +1000

    create dice | style dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import './App.css';
+import Die from './assets/components/Die';
 
 function App() {
-  return <main></main>;
+  let i = 1;
+
+  const values = [...Array(10).keys()].map((die) => {
+    if (i > 6) i = 1;
+    if (die > 5) {
+      const newNumber = 0 + i;
+      i++;
+      return newNumber;
+    }
+    return die + 1;
+  });
+
+  const dice = values.map((die) => <Die value={die} />);
+  return (
+    <main>
+      <div className="die-container">{dice}</div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/assets/components/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die({ value }) {
+  return (
+    <div className="die-face">
+      <h2 className="die-num">{value}</h2>
+    </div>
+  );
+}
