abc2718b1d3f66ee61a6aac0c0bc8e1b274e634a
rayves <vo.raymond.v@outlook.com>
2022-08-20 02:20:50 +1000

    adds confetti package | add confetti effect on win

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import Die from './assets/components/Die';
 import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti';
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
@@ -67,6 +68,7 @@ function App() {
 
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
@@ -81,7 +83,7 @@ function App() {
           counter();
         }}
       >
-        Roll
+        {tenzies ? 'New Game' : 'Roll'}
       </button>
     </main>
   );
